3b6644676d0d006bba3c4ca910faf0b452086ea7
SiderealArt <30827929+SiderealArt@users.noreply.github.com>
2022-05-09 15:57:00 +0800

    npm init

---
+++ index.js
@@ -0,0 +1,22 @@
+const playwright = require('playwright');
+async function main() {
+    const browser = await playwright.chromium.launch()
+    const page = await browser.newPage();
+    await page.goto('https://qry.nfu.edu.tw/classname.php');
+    await page.fill('#coursename', '%');
+    await page.click('#bt_qry');
+    await page.waitForLoadState('networkidle');
+    const rows = page.locator('table tr');
+    /*const texts = await rows.evaluateAll(list => list.map(element => {
+        if (element.querySelector(':nth-child(3)'))
+            return element.querySelector(':nth-child(3)').innerText;
+    }));*/
+
+    const count = await rows.count()
+    for (let i = 4; i < count; ++i)
+        console.log("類型：",await rows.nth(i).locator(':nth-child(3)').innerText(), " 名稱：", await rows.nth(i).locator(':nth-child(2)').innerText(), " 教師：", await rows.nth(i).locator(':nth-child(7)').innerText(), " 學分：", await rows.nth(i).locator(':nth-child(4)').innerText() ," 地點：", await rows.nth(i).locator(':nth-child(15)').innerText());
+    await browser.close();
+
+}
+
+main();
---
+++ template.js
@@ -0,0 +1,10 @@
+const a = {
+    "id": 1,
+    "name": "機器視覺與影像處理",
+    "teacher": "卓文祥",
+    "type": 1, //1: 必修 2: 選修 3: 通識
+    "credit": 3,
+    "class": "碩資一甲",
+    "location": "BGA0340",
+    "time": [6,9,12]
+} 
