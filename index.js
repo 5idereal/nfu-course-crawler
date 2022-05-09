219acac0960a15d267d882a49e86a73f76c7ede1
SiderealArt <30827929+SiderealArt@users.noreply.github.com>
2022-05-09 16:12:08 +0800

    regex for courses after 106 semester

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ async function main() {
     }));*/
 
     const count = await rows.count()
-    for (let i = 4; i < count; ++i)
-        console.log("類型：",await rows.nth(i).locator(':nth-child(3)').innerText(), " 名稱：", await rows.nth(i).locator(':nth-child(2)').innerText(), " 教師：", await rows.nth(i).locator(':nth-child(7)').innerText(), " 學分：", await rows.nth(i).locator(':nth-child(4)').innerText() ," 地點：", await rows.nth(i).locator(':nth-child(15)').innerText());
+    for (let i = 3; i < count; ++i)
+        //console.log("類型：",await rows.nth(i).locator(':nth-child(3)').innerText(), " 名稱：", await rows.nth(i).locator(':nth-child(2)').innerText(), " 教師：", await rows.nth(i).locator(':nth-child(7)').innerText(), " 學分：", await rows.nth(i).locator(':nth-child(4)').innerText() ," 地點：", await rows.nth(i).locator(':nth-child(15)').innerText());
+        console.log(" 地點：",/([A-Z])\w+([A-Z])\w+/.exec(await rows.nth(i).locator(':nth-child(15)').innerText())[0]);
     await browser.close();
 
 }
