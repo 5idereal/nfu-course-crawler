21cd4e4d5c80d9ead9cba0cd2f0ae85e0c0a673a
SiderealArt <30827929+SiderealArt@users.noreply.github.com>
2022-05-09 23:18:06 +0800

    update readme & crawl all semester

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # nfu-course-crawler
+
+# 關於
+
+# 開始使用
+
+## 前往網頁
+
+## 檢視原始檔案
+
+### 屬性
+
+- **id** *string*: **課號** 
+
+- **name** *string*: **課程名稱**
+
+- **teacher** *string*: **老師姓名**
+
+- **type** *number*: **課程類型** (1: 必修, 2: 選修, 3: 通識)
+
+- **credit** *number*: **學分數**
+
+- **hours** *string*: **時數**
+
+- **class** *string*: **授課班級**
+
+- **M~U** *string*: **節數** (M~U 對應 星期一到日)
+
+- **location** *string*: **上課地點**
+
+- **students** *array*: **學生名單** (學號)
+
+# 自行爬取
+
+> 請確認是否已安裝 [Node.js](https://nodejs.org/en/) 和 [git](https://git-scm.com/)。
+
+1. 將此 repo 下載到電腦上。
+
+```bash
+git clone https://github.com/SiderealArt/nfu-course-crawler
+cd nfu-course-crawler
+```
+
+2. 安裝所需套件。
+
+```bash
+npm i
+```
+
+3. 執行程式。
+
+```bash
+node index.js
+```
+
+> ***爬取所有資料預計將耗費 18 小時。***
+
+## 致謝
--- test.js
+++ test.js
@@ -1,43 +1,49 @@
 const playwright = require('playwright');
 const cliProgress = require('cli-progress');
 const jsonfile = require('jsonfile');
-const { json } = require('stream/consumers');
 async function main() {
-    let arr = [];
-    console.log("正在取得 107 學年度第 2 學期課程資料...");
-    const browser = await playwright.chromium.launch()
+    console.log("🕓 網頁載入中...");
+    const browser = await playwright.chromium.launch();
     const page = await browser.newPage();
     await page.goto('https://qry.nfu.edu.tw/classname.php');
-    await page.selectOption('select#selyr', '1072');
     await page.fill('#coursename', '%');
-    await page.click('#bt_qry');
-    await page.waitForLoadState('networkidle');
-    const rows = page.locator('table tr');
-    /*const texts = await rows.evaluateAll(list => list.map(element => {
-        if (element.querySelector(':nth-child(3)'))
-            return element.querySelector(':nth-child(3)').innerText;
-    }));*/
-    const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-   // const count = await rows.count();
-const count = 100;
-    bar1.start(count-3, 0);
-    for (let i = 3; i < count; ++i)
-    {
-        arr.push({
-            "id": await rows.nth(i).locator(':nth-child(1) > a').innerText(),
-            "name": await rows.nth(i).locator(':nth-child(2)').innerText(),
-            "teacher": await rows.nth(i).locator(':nth-child(7)').innerText(),
-            "type": await rows.nth(i).locator(':nth-child(3)').innerText() == "必修" ? 1 : await rows.nth(i).locator(':nth-child(3)').innerText() == "選修" ? 2 : 3,
-            "credit": await rows.nth(i).locator(':nth-child(4)').innerText(),
-            "class": await rows.nth(i).locator(':nth-child(5)').innerText(),
-            "location": /[A-Z0-9]{3,}/.exec(await rows.nth(i).locator(':nth-child(15)').innerText())[0]
-        });
-        bar1.increment();
-        
-    };
-    bar1.stop();
-    jsonfile.writeFileSync('data.json', arr);
-    console.log("完成！");
+    const options = page.locator('select > option');
+    const options_count = await options.count();
+    for (let i = 1; i < options_count; ++i) {
+        let arr = [];
+        page.selectOption('select#selyr', await options.nth(i).getAttribute("value"));
+        await page.click('#bt_qry');
+        await page.waitForLoadState('networkidle');
+        console.log(`(${i}/${options_count}) 🏫 正在取得${await options.nth(i).innerText()}的課程資料...`);
+        const rows = page.locator('table tr');
+        const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+        const count = /*await rows.count()*/ 5;
+        bar1.start(count - 3, 0);
+        for (let i = 3; i < count; ++i) {
+            arr.push({
+                "id": await rows.nth(i).locator(':nth-child(1) > a').innerText(),
+                "name": await rows.nth(i).locator(':nth-child(2)').innerText(),
+                "teacher": await rows.nth(i).locator(':nth-child(7)').innerText(),
+                "type": await rows.nth(i).locator(':nth-child(3)').innerText() == "必修" ? 1 : await rows.nth(i).locator(':nth-child(3)').innerText() == "選修" ? 2 : 3,
+                "credit": await rows.nth(i).locator(':nth-child(4)').innerText(),
+                "hours": await rows.nth(i).locator(':nth-child(5)').innerText(),
+                "class": await rows.nth(i).locator(':nth-child(6)').innerText(),
+                "M": await rows.nth(i).locator(':nth-child(8)').innerText(),
+                "T": await rows.nth(i).locator(':nth-child(9)').innerText(),
+                "W": await rows.nth(i).locator(':nth-child(10)').innerText(),
+                "R": await rows.nth(i).locator(':nth-child(11)').innerText(),
+                "F": await rows.nth(i).locator(':nth-child(12)').innerText(),
+                "S": await rows.nth(i).locator(':nth-child(13)').innerText(),
+                "U": await rows.nth(i).locator(':nth-child(14)').innerText(),
+                "location": /[A-Z0-9]{3,}/.exec(await rows.nth(i).locator(':nth-child(15)').innerText())[0],
+                "students": ["a"]
+            });
+            bar1.increment();
+        };
+        bar1.stop();
+        jsonfile.writeFileSync(`./data/${await options.nth(i).getAttribute("value")}.json`, arr);
+    }
+    console.log("👌 完成！");
     await browser.close();
 
 }
